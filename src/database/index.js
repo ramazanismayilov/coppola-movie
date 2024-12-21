22bb9a9b547827e447763b01672e41f91a134f16
unknown <ismayiloff522@gmail.com>
2024-12-21 18:24:07 +0400

    pg

--- src/database/index.js
+++ src/database/index.js
@@ -1,7 +1,21 @@
 const { Sequelize } = require("sequelize");
 const config = require("../config");
 
-const sequelize = new Sequelize(config.databaseUrl);
+const sequelize = new Sequelize(config.databaseUrl, {
+  pool: {
+    max: 5, // Maksimum bağlantı sayısı
+    min: 0, // Minimum bağlantı sayısı
+    acquire: 30000, // Maksimum bekleme süresi
+    idle: 10000, // Bağlantının boşta kalma süresi
+  },
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
+
 
 sequelize
   .authenticate()
