65bf9e6e382a447ad97b9adb732df24c91e545c5
unknown <ismayiloff522@gmail.com>
2024-12-22 13:11:35 +0400

    change package

--- src/database/index.js
+++ src/database/index.js
@@ -1,10 +1,7 @@
 const { Sequelize } = require("sequelize");
 const config = require("../config");
 
-const sequelize = new Sequelize(config.databaseUrl, {
-  dialect: "postgres", 
-  logging: false, 
-});
+const sequelize = new Sequelize(config.databaseUrl, { logging: false });
 
 sequelize
   .authenticate()
