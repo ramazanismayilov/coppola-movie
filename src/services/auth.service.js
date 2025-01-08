7414791fdf6ea2fb72e3d9ea1ba64c4f39842bea
unknown <ramazandev2024@gmail.com>
2025-01-08 14:24:44 +0400

    test

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -32,12 +32,13 @@ const login = async (params) => {
 const register = async (params) => {
   const existingUser = await User.findOne({ email: params.email });
   if (existingUser) {
-    throw new ConflictError(`This email or username already exists.`);
+    throw new ConflictError(`This email already exists.`);
   }
 
   const hashedPassword = await bcrypt.hash(params.password, 10);
 
   const user = new User({ ...params, password: hashedPassword });
+  console.log(user)
 
   const mailContent = await renderTemplate("welcome-mail", {
     user: user.toJSON(),
