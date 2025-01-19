ca90f3534017b50448f39788886f71125b7f8cb6
unknown <ramazandev2024@gmail.com>
2025-01-19 19:39:48 +0400

    static

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 app.use(express.json());
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use('/api/uploads', express.static(path.join(__dirname, 'uploads')));
 
 //* routes
 app.use("/api", router);
